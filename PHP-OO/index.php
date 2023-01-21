2dabb6109a2154f2e9949b021d47cc963bf78afb
Camila da Costa <camila.coosta99@gmail.com>
2023-01-21 02:02:50 -0300

    aula04 - construct method and use case

--- PHP-OO/index.php
+++ PHP-OO/index.php
@@ -3,6 +3,14 @@
 class Login{
     private $email;
     private $senha;
+    private $nome;
+
+    public function __construct($email, $senha, $nome)//método construtor
+    {
+        $this->nome = $nome;
+        $this->setEmail($email);
+        $this->setSenha($senha);
+    }
 
     public function setEmail($e){
         $email = filter_var($e, FILTER_SANITIZE_EMAIL);//seta o dado passado como parametro na variavel da class e usa filter_var para excluir dados fora do padrão
@@ -19,6 +27,10 @@ class Login{
         return $this->senha;
     }
 
+    public function getNome(){
+        return $this->nome;
+    }
+
     public function Logar(){
         if($this->email == "teste@teste.com" && $this->senha == "123456"){
             echo "Logado com sucesso<br>";
@@ -28,10 +40,7 @@ class Login{
     }
 }
 
-$login = new Login();
-
-$login->setEmail("teste@teste.com");
-$login->setSenha("123456");
+$login = new Login("teste@teste.com","123456","camila");
 $login->Logar();
 
-echo $login->getEmail() .' | '. $login->getSenha();
+echo $login->getEmail() .' | '. $login->getSenha() ." | ".$login->getNome();
