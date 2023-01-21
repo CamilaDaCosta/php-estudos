4787c163e368a18e60d9052eb81c98e1ea0c17ed
Camila da Costa <camila.coosta99@gmail.com>
2023-01-21 18:50:21 -0300

    aula18 - composition

--- PHP-OO/index.php
+++ PHP-OO/index.php
@@ -1,43 +1,29 @@
 <?php
 
 /**
- * AGREGAÇÃO: quando uma classe precisa de outra para executar sua ação
- * uma classe que usa outra como parte de si mesma
+ * COMPOSIÇÃO: uma classe cria a instancia de outra classe dentro de si
+ * mesma, quando ela for destruída a instancia da outra classe também será
  */
 
-class Produtos{
-    public $name;
-    public $valor;
-
-    public function __construct($name, $valor){
-        $this->name = $name;
-        $this->valor = $valor;
+class Pessoa{
+    public function atribuiNome($nome){
+        return "O nome da pessoa é {$nome}";
     }
 }
 
-class Carrinho{
-    public $produtos;
+class Exibe{
+    public $pessoa;
+    public $nome;
 
-    public function adiciona(Produtos $produto){//agregacao
-        $this->produtos[] = $produto;
+    function __construct($nome){
+        $this->pessoa = new Pessoa();//composição
+        $this->nome = $nome;
     }
 
-    public function exibir(){
-        foreach($this->produtos as $produto ){
-            echo $produto->name . "<br>";
-            echo $produto->valor . "<hr>";
-        }
+    public function exibeNome(){
+        echo $this->pessoa->atribuiNome($this->nome);
     }
 }
 
-//$produto1 = new Produtos("P1",1200);
-$produto2 = new Produtos("P2",100);
-$produto3 = new Produtos("P3",80);
-
-$carrinho = new Carrinho();
-$carrinho->adiciona(new Produtos("P1",1200));
-$carrinho->adiciona($produto2);
-$carrinho->adiciona($produto3);
-
-$carrinho->exibir();
-
+$exibe = new Exibe("Camila");
+$exibe->exibeNome();
