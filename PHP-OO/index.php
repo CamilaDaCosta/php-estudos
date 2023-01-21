a2bf28bc500fdde7495ea8a4d77d6ebd3051119d
Camila da Costa <camila.coosta99@gmail.com>
2023-01-21 04:40:58 -0300

    aula09 - const type | when use self and parent

--- PHP-OO/index.php
+++ PHP-OO/index.php
@@ -1,41 +1,22 @@
 <?php
-/**
- * ABSTRATA: define regras que as classes que a implementarem devem 
- * seguir obrigatoriamente
- */
 
-abstract class Banco{
-    protected $saldo;
-    protected $limiteSaque;
-    protected $juros;
+class Pessoa{
+    const NOME = "Camila";
 
-    abstract protected function Sacar($s);
-    abstract protected function Depositar($d);
-
-    public function setSaldo($s){
-        $this->saldo = $s;
-    }
-
-    public function getSaldo(){return $this->saldo;}
-}
-
-class Banco1 extends Banco{
-    public function Sacar($s)
-    {
-        $this->saldo -= $s;
-        echo "<hr>- {$s} [Saque]";
-    }
-    public function Depositar($d)
-    {
-        $this->saldo += $d;
-        echo "<hr>+ {$d} [Depósito]";
+    public function exibirNome(){
+        echo self::NOME;
     }
 }
 
-$b1 = new Banco1();
-$b1->setSaldo(1000);
-echo "<hr>Saldo = {$b1->getSaldo()}";
-$b1->Sacar(200);
-echo "<hr>Saldo = {$b1->getSaldo()}";
-$b1->Depositar(500);
-echo "<hr>Saldo = {$b1->getSaldo()}";
+class Camila extends Pessoa{
+    const NOME = "Costa";
+
+    public function exibirNome(){
+        echo self::NOME;//do mesmo escopo
+        echo parent::NOME;//do escopo herdado
+    }
+
+}
+
+$pessoa = new Camila();
+$pessoa->exibirNome();
