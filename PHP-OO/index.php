7415a3426e51cd05776b1c6d663ef258495a7b39
Camila da Costa <camila.coosta99@gmail.com>
2023-01-21 01:54:12 -0300

    aula03 - getters and setters

--- PHP-OO/index.php
+++ PHP-OO/index.php
@@ -1,15 +1,37 @@
 <?php
 
-class Pessoa{ //cria class
-    public $nome;
-    public $idade;
+class Login{
+    private $email;
+    private $senha;
 
-    public function falar(){//cria method
-        echo $this->nome . " de ". $this->idade . " anos";
-    }//method
-}//class
+    public function setEmail($e){
+        $email = filter_var($e, FILTER_SANITIZE_EMAIL);//seta o dado passado como parametro na variavel da class e usa filter_var para excluir dados fora do padrão
+        $this->email = $email;
+    }
+    public function getEmail(){
+        return $this->email;//acessa o dado private
+    }
 
-$pessoa = new Pessoa();
-$pessoa->nome = "Camila da Costa";
-$pessoa->idade = "23";
-$pessoa->falar();
+    public function setSenha($e){
+        $this->senha = $e;
+    }
+    public function getSenha(){
+        return $this->senha;
+    }
+
+    public function Logar(){
+        if($this->email == "teste@teste.com" && $this->senha == "123456"){
+            echo "Logado com sucesso<br>";
+        }else{
+            echo "Dados Invalidos";
+        }
+    }
+}
+
+$login = new Login();
+
+$login->setEmail("teste@teste.com");
+$login->setSenha("123456");
+$login->Logar();
+
+echo $login->getEmail() .' | '. $login->getSenha();
