a4926ecfbb4e51d6c89b8fd7459d969ed76d92b6
Camila da Costa <camila.coosta99@gmail.com>
2023-01-21 05:09:22 -0300

    aula12 - interfaces

--- PHP-OO/index.php
+++ PHP-OO/index.php
@@ -1,32 +1,39 @@
 <?php
 
 /**
- * POLIMORFISMO: sobreescrever um metodo herdadado
- * @Override
+ * INTERFACE: contrato que garante que as classes que a implementarem terão
+ * que implementar os metodos que existem nela
+ * 
+ * Em resumo, uma interface é uma forma de garantir que as classes que 
+ * a implementam possuam determinados métodos, enquanto uma classe abstrata
+ * é uma forma de fornecer uma implementação básica para as classes que a estendem.
+ * Uma classe pode implementar múltiplas interfaces, mas só pode estender 
+ * uma única classe abstrata. 
+ * Além disso, uma interface não pode conter atributos ou métodos com 
+ * implementação, enquanto uma classe abstrata pode.
  */
-
-class Animal{
-    public function Andar(){
-        echo "Animal andou";
-    }
-
-    public function Correr(){
-        echo "Animal correu";
-    }
+interface Crud{
+    public function create();
+    public function read();
+    public function update();
+    public function delete();
 }
 
-class Cavalo extends Animal{
-    public function Andar(){
-        echo "Cavalo andou";
-        $this->Correr();
+class Noticias implements Crud{
+    public function create()
+    {
+        
+    }
+    public function read()
+    {
+        
+    }
+    public function update()
+    {
+        
+    }
+    public function delete()
+    {
+        
     }
 }
-
-$animal = new Cavalo();
-$animal->Andar();
-
-echo "<hr>";
-
-$animal = new Animal();
-$animal->Andar();
-$animal->Correr();
