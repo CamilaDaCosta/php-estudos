c8f6caedb226d9f1c9ebb27f1c2fcfa74b27f90f
Camila da Costa <camila.coosta99@gmail.com>
2023-11-22 01:19:13 -0300

    Adicionando carrinho com função de add e remove

--- VUE3/js/app.js
+++ VUE3/js/app.js
@@ -3,18 +3,34 @@ const app = {
         return{
             name: 'Camila',
             lastName: 'Costa',
-            films:{
-                title: 'Persona',
-                image: './assets/images/persona-bergman.jpg',
-                stars: 4.5,
-            },
-            films2:{
-                title: 'The Godfather',
-                image: './assets/images/godfather.jpg',
-                stars: 5,
-            }
-        }
-    }
+            films: [
+                {
+                    title: 'Persona',
+                    image: './assets/images/persona-bergman.jpg',
+                    stars: 4.5,
+                },
+                {            
+                    title: 'The Godfather',
+                    image: './assets/images/godfather.jpg',
+                    stars: 5,
+                }
+            ],
+            cart: [],
+        }        
+    },
+    methods: {
+        addCart(film){
+            this.cart.push(film);
+        },
+
+        inCart(film){
+            return this.cart.indexOf(film) != -1;
+        },
+
+        removeCart(film){
+            this.cart = this.cart.filter((value, index) => film != value)
+        },
+    },
 }
 
 Vue.createApp(app).mount("#app");
